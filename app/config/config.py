61f5c3338d1bded5ca7d37dc346818f84dfafb72
EuniceGeorge <georgeunice408.com>
2025-01-02 15:44:15 +0100

    update

--- app/config/config.py
+++ app/config/config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 
 # hows how to initialize and configure a simple SQLite database. 'mysql://username:password@host:port/database_name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://MedEase_user:medease@localhost/MedEase'
