b6f00150cf143db7c89a4d99b21c843588c81b04
EuniceGeorge <georgeunice408.com>
2025-01-03 14:30:23 +0100

    u

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask
-
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 
 app = Flask(__name__)
-app.debug = True
 
 # hows how to initialize and configure a simple SQLite database. 'mysql://username:password@host:port/database_name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://MedEase_user:medease@localhost/MedEase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #db object instantiated from the class SQLAlchemy
-#..........................
-
+db = SQLAlchemy(app)
 
 
 # testing the apri service
