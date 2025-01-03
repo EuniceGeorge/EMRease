bc0a42c903bf517fa9fe354ce02aaf327bf7aab4
EuniceGeorge <georgeunice408.com>
2025-01-03 14:31:40 +0100

    tsk

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# hows how to initialize and configure a simple SQLite database. 'mysql://username:password@host:port/database_name
+# hows how to initialize and configure a simple mysql database. 'mysql://username:password@host:port/database_name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://MedEase_user:medease@localhost/MedEase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
