c4d327bd17ad337d67454734e38f626f39763313
EuniceGeorge <georgeunice408.com>
2025-01-03 13:00:25 +0100

    update

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,17 +1,7 @@
 from flask import Flask
-from config.config import app
-from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-
-# hows how to initialize and configure a simple SQLite database. 'mysql://username:password@host:port/database_name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://MedEase_user:medease@localhost/MedEase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to use less memory unless signals for object changes are needed.
-
-#db object instantiated from the class SQLAlchemy
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
