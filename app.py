5a07fc92c754415db1409a89ef88048ec73c2ffa
EuniceGeorge <georgeunice408.com>
2025-01-03 14:24:49 +0100

    u

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
-# from models import db, Patient, Doctor, MedicalRecord, Appointment
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.debug = True
+
+# hows how to initialize and configure a simple SQLite database. 'mysql://username:password@host:port/database_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://MedEase_user:medease@localhost/MedEase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#db object instantiated from the class SQLAlchemy
+#..........................
+
+
 
 # testing the apri service
 @app.route('/')
