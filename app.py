49992dfb059b606fa29915d268c94d70ef3be030
EuniceGeorge <georgeunice408.com>
2025-01-03 13:45:04 +0100

    update

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# testing the apri service
+@app.route('/')
+def index():
+    return "Hello world !!!!"
+
+@app.route('/user/<name>')
+def user(name):
+    # return '<h1>Hello, {}!</h1>'.format(name)
+    return f'<h1> Hello, {name}</h1>.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
